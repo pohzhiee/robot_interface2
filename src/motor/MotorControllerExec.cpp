84835754033580b5a9e40191af3c15dd69a4c009
Poh Zhi-Ee <ZhiEe-Poh@ecosysgrp.com>
2021-12-14 18:10:57 +0800

    Change MotorController to use DMASPI

--- src/motor/MotorControllerExec.cpp
+++ src/motor/MotorControllerExec.cpp
@@ -1,11 +1,13 @@
+#include "raspiHardware/DMASPI.hpp"
 #include "raspiHardware/GPIO.hpp"
 #include "raspiHardware/Mmap.hpp"
 #include "raspiHardware/SPI.hpp"
-#include "robot_interface2/motor/MotorController.hpp"
 #include "raspiHardware/SimultaneousSPI.hpp"
+#include "robot_interface2/motor/MotorController.hpp"
 #include <ecal/ecal.h>
 #include <iostream>
 
+using namespace robot_interface2;
 int main()
 {
     // initialize eCAL API
@@ -15,9 +17,18 @@ int main()
     eCAL::Process::SetState(proc_sev_healthy, proc_sev_level1, "Robot1 Motor Controller");
 
     uintptr_t gpioMmapPtr = mmapPeriph(GPIO_Base);
+    uintptr_t dmaMmapPtr = mmapPeriph(DMA_Base);
     uintptr_t spiMmapPtr = mmapPeriph(SPI_Base);
+    if (gpioMmapPtr == 0)
+    {
+        std::cerr << "Failed to mmap gpio" << std::endl;
+        return -1;
+    }
     auto simulSpi = std::make_shared<SimultaneousSPI>(SPISettings{}, gpioMmapPtr, spiMmapPtr);
-    auto a = robot_interface2::MotorController(std::move(simulSpi));
+    std::array<DMASPISetting, 2> spiSetting = {DMASPISetting{.SPI_CLK = 20, .rxDmaNum = 0, .txDmaNum = 1},
+                                               DMASPISetting{.SPI_CLK = 20, .rxDmaNum = 2, .txDmaNum = 3}};
+    auto dmaSpi = std::make_shared<DMASPI>(spiSetting, gpioMmapPtr, dmaMmapPtr, spiMmapPtr);
+    auto a = robot_interface2::MotorController(std::move(dmaSpi));
     while (eCAL::Ok())
     {
         eCAL::Process::SleepMS(100);
